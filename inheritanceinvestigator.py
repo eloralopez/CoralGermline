9bdaff70d8ce41f3b24b3b4f17da6aeb1439e8ef
ehlopez5 <elora@stanford.edu>
2020-03-11 12:20:09 -0700

    Update inheritanceinvestigator.py

--- inheritanceinvestigator.py
+++ inheritanceinvestigator.py
@@ -6,36 +6,67 @@ import sys
 import re
 
 input1 = sys.argv[1]
-replicate1 = int(sys.argv[2])
-replicate2 = int(sys.argv[3])
-replicate3 = int(sys.argv[4]) #THIS IS THE SAMPLE YOU WILL LOOK FOR UNIQUE MUTATIONS IN
-replicate4 = int(sys.argv[5]) #THIS IS THE SAMPLE YOU WILL LOOK FOR UNIQUE MUTATIONS IN
-numberofparents = int(sys.argv[6]) #the number of parent samples in the VCF.
-spermrep1 = int(sys.argv[7]) # the position of the desired sperm sample in the list of sample columns
-spermrep2 = int(sys.argv[8]) # the position of the desired sperm sample in the list of sample columns
+replicate1 = int(sys.argv[2]) #column number for the first replicate of the first parent ie CAP22-1
+replicate2 = int(sys.argv[3]) #column number for the second replicate of the first parent ie CAP22-2
 
-def fileinfo(inputfile, rep1, rep2): 
+# replicate3 = int(sys.argv[4]) #THIS IS THE SAMPLE YOU WILL LOOK FOR UNIQUE MUTATIONS IN
+# replicate4 = int(sys.argv[5]) #THIS IS THE SAMPLE YOU WILL LOOK FOR UNIQUE MUTATIONS IN
+numberofparents = int(sys.argv[4]) #the number of parent samples in the VCF.
+# spermrep1 = int(sys.argv[7]) # the position of the desired sperm sample in the list of sample columns
+# spermrep2 = int(sys.argv[8]) # the position of the desired sperm sample in the list of sample columns
+replicate3= replicate1 + 2
+
+replicate4 = replicate2 + 2
+
+replicate5 = replicate3 + 2
+
+replicate6 = replicate4 + 2
+
+replicate7 = replicate5 + 2
+
+replicate8 = replicate6 + 2
+
+spermrep1 = replicate1 + 8
+
+spermrep2 = replicate2 + 8
+
+spermrep3 = replicate3 + 8
+
+spermrep4 = replicate4 + 8
+
+spermrep5 = replicate5 + 8
+
+spermrep6 = replicate6 + 8
+
+spermrep7 = replicate7 + 8
+
+spermrep8 = replicate8 + 8
+
+#print(replicate1, replicate2, replicate3, replicate4, replicate5, replicate6, replicate7, replicate8)
+#print(spermrep1, spermrep2, spermrep3, spermrep4, spermrep5, spermrep6, spermrep7, spermrep8)
+
+def fileinfo(inputfile, rep1, rep2):
     listofoutlist = [] #starts an empty list to which the matches between replicate libraries will be added later
-    
+
     with open(inputfile, 'r') as f: #opens whichever file is specified
-        
+
         for line in f:
-            if line.startswith('#'):
+            if line.startswith('##'): #ignores all the header lines
                 continue
-            
-            elif line.startswith('#CHROM'): #ignores all the header lines
+
+            if line.startswith('#CHROM'): #ignores all the header lines
 
                 line = line.strip()
 
                 items = line.split('\t')
-                
+
                 samplenames = items[9:]
                 samplenames = '\t'.join(samplenames)
                 #print(samplenames)
                 header = ["chrom.pos", "ref", "alt", samplenames, samplenames]
                 #print(header)
                 header = '\t'.join(header)
-               # print(header)
+                #print(header)
                 #print(samplenames_out)
                 listofoutlist.append(header)
                 #header = "Chrom.pos","ref", "alt", "sample1","sample2","sample3","sample4","sample5","sample6","sample7", "sample8", "sample9", "sample10", "sample11", "sample12", "TrueorFalse"
@@ -64,25 +95,25 @@ def fileinfo(inputfile, rep1, rep2):
                 geno_list=[] #empty list to which genotypes will be added later
 
                 genoplusdepthlist=[] #empty list to which genotype depths will be added later
-                
+
                 for genotype in genotypes:
 
                     genos = genotype.split(':') #splits the genotype column up by ":"
                     alleles = re.split('; |, |\||\/',genos[0])#genos[0].split('/') #splits the first part of the genotype column into its two respective alleles
                     #print(alleles)
                     GQscore = genos[3]
-                    
+
                     alleledepths = genos[1].split(',') #depth per allele at locus
                     #FROM GATK: AD is the unfiltered allele depth, i.e. the number of reads that support each of the reported alleles. All reads at the position (including reads that did not pass the variant caller’s filters) are included in this number, except reads that were considered uninformative. Reads are considered uninformative when they do not provide enough statistical evidence to support one allele over another.
 
                     refdepth = alleledepths[0]
-                    
+
                     altdepth = alleledepths[1]
 
 #                    totaldepth_atlocus = genos[2] #totaldepth at locus
 
                     #FROM GATK: DP is the filtered depth, at the sample level. This gives you the number of filtered reads that support each of the reported alleles. You can check the variant caller’s documentation to see which filters are applied by default. Only reads that passed the variant caller’s filters are included in this number. However, unlike the AD calculation, uninformative reads are included in DP.
-                    
+
                     if alleles[0] == ".": #ignore the sites where the depth is './.'
                         continue
                     if int(alleles[0]) == 0:
@@ -109,26 +140,39 @@ def fileinfo(inputfile, rep1, rep2):
 
                     geno_list.append(genostring)
                     genostring = '\t'.join(geno_list)
-                    
+
                 if geno_list[rep1] == geno_list[rep2]: #outputs just the sites where the two replicate libraries are the same genotype
                 #if all(x==geno_list[0] for x in geno_list):
                     outlist = [concatenated, ref_allele, alt_allele, genostring, genoPLUSdepth] # collect each of these for each line where the genotypes are the same
-                    outstring = '\t'.join(outlist) 
+                    outstring = '\t'.join(outlist)
 
                     listofoutlist.append(outstring) #append each line to the list started up at the top
                     #print(concatenated, outlist)
                 #else:
                     #continue #skips all sites that have different genotype calls betweeen the two technical replicates
     return listofoutlist
+    print(listoutoutlist)
     return dictionary #returns the full list of genotype matches in the file
 
     f.close()
 
-genos1=fileinfo(input1, replicate1, replicate2) #calls the function for a given set of replicate libraries (whichever 2 columns you want)   #FLAG
+genos1=fileinfo(input1, replicate1, replicate2) #CAP22 #calls the function for a given set of replicate libraries (whichever 2 columns you want)   #FLAG
 #print(genos1)
-genos2=fileinfo(input1, replicate3, replicate4) #calls the function for CAP24
+genos2=fileinfo(input1, replicate3, replicate4) #CAP23
 
-spermgenos=fileinfo(input1, spermrep1, spermrep2)
+genos3=fileinfo(input1, replicate5, replicate6) #CAP24
+
+genos4=fileinfo(input1, replicate7, replicate8) #CAP26
+ #calls the function for CAP24
+#print(genos2)
+spermgenos=fileinfo(input1, spermrep1, spermrep2) #CAS22
+
+spermgenos2=fileinfo(input1, spermrep3, spermrep4) #CAS23
+
+spermgenos3=fileinfo(input1, spermrep5, spermrep6) #CAS24
+
+spermgenos4=fileinfo(input1, spermrep7, spermrep8) #CAS26
+
 #print(spermgenos)
 #print(genos2)
 # genos3=fileinfo(input1, 4, 5)
@@ -143,9 +187,9 @@ def makedictionary(fileinfo_output, replicatenumber):
     geno1list = []
     for line1 in fileinfo_output: #goes line by line in the first input file's list of matches
             #print(line1)
-            if line1.startswith('CAP23'):
+            if line1.startswith('chrom.pos'):
                 continue
-            else: 
+            else:
                 line1 = line1.strip()
 
                 items1 = line1.split('\t')
@@ -166,9 +210,18 @@ def makedictionary(fileinfo_output, replicatenumber):
     return dictionary
 
 dictOfWords = makedictionary(genos1, replicate1)
+dictOfWords2 = makedictionary(genos2, replicate3)
+dictOfWords3 = makedictionary(genos3, replicate5)
+dictOfWords4 = makedictionary(genos4, replicate7)
+
+
 #print(dictOfWords)
 
 spermdict = makedictionary(spermgenos, spermrep1)
+spermdict2 = makedictionary(spermgenos2, spermrep3)
+spermdict3 = makedictionary(spermgenos3, spermrep5)
+spermdict4 = makedictionary(spermgenos4, spermrep7)
+
 #print(spermdict)
 #print(dictOfWords)
 # conc3list = []
@@ -218,21 +271,22 @@ spermdict = makedictionary(spermgenos, spermrep1)
 conc2list = []
 geno2list = []
 
-
+#to find CAP23 mutants:
 for line2 in genos2: #now goes line by line in the list of matches from the second set of replicates. this set is the MUTANT set.
-
+        #print(line2)
         if line2.startswith('chrom.pos'):
+            #print(line2)
             line2 = line2.strip()
 
             items2 = line2.split('\t')
             total = (numberofparents * 2) + 3 #this gives the number of columns from the first line to output. It is the sum of the number of parent samples, the number of sperm samples (Which is equal to the number of parent samples), and the three columns that denote chrom.pos, ref, and alt
             header = items2[0:total]
             header = '\t'.join(header)
-            finalheader = [header, "TrueorFalse"]
+            finalheader = [header, "TypeofMutation", "TrueorFalse"]
             finalheader = '\t'.join(finalheader)
-            
+
             print(finalheader) # this is the headerline for the file
-        else:    
+        else:
             line2 = line2.strip()
 
             items2 = line2.split('\t')
@@ -245,30 +299,30 @@ for line2 in genos2: #now goes line by line in the list of matches from the seco
             genotypes2= items2[3:]
             #print(genotypes2)
             geno2= genotypes2[replicate3]
-            #print(genotypes2)
+
+            #First deal with somatic mutations:
             if conc2 in dictOfWords:
                 geno1 = dictOfWords[conc2] #outputs just the geno1 values that match conc2 (that is, just the sites that are present in the lists from genos1 and genos2)
                 #print(geno1)
-                #print(geno2)
-                # geno3 = dictOfWords3[conc2]
-                # geno4 = dictOfWords4[conc2]
-                #if geno2 != geno1
+
                 if geno1 != geno2 and geno2 != genotypes2[4] and geno2 != genotypes2[5] and geno2 != genotypes2[6] and geno2 != genotypes2[7]: #this indicates that the mutation found in replicate3 and replicate4 (now referred to as geno2) is UNIQUE among the parent samples; that genotype is never seen at that site in any other parent sample
                     genolist = genotypes2[7:]
-                    #print(genotypes2)
-                    if genotypes2[replicate3] == genotypes2[spermrep1] == genotypes2[spermrep2]: #if the corresponding sperm pools are the same genotype as the mutant parent, inheritance is true. If not, inheritance is false.
+                    Type_of_Mutation = "SomaticMutation"
+                    if genotypes2[replicate3] == genotypes2[spermrep3] == genotypes2[spermrep4]: #if the corresponding sperm pools are the same genotype as the mutant parent, inheritance is true. If not, inheritance is false.
                         #print("yaaa")
                         Match = True
                     else:
+                        #perhaps add extra conditions to deal with the low minor allele frequency sperm genotypes?
                         Match = False
-                    writeout = [conc2, ref, alt, genotypes2[16],genotypes2[17], genotypes2[18], genotypes2[19], genotypes2[20], genotypes2[21], genotypes2[22], genotypes2[23], genotypes2[24], genotypes2[25], genotypes2[26], genotypes2[27], genotypes2[28], genotypes2[29], genotypes2[30], genotypes2[31], str(Match)]
+                    writeout = [conc2, ref, alt, genotypes2[16],genotypes2[17], genotypes2[18], genotypes2[19], genotypes2[20], genotypes2[21], genotypes2[22], genotypes2[23], genotypes2[24], genotypes2[25], genotypes2[26], genotypes2[27], genotypes2[28], genotypes2[29], genotypes2[30], genotypes2[31], str(Match), str(Type_of_Mutation)]
                     #here is your writeout of all of the somatic mutations unique to a particular branch
                     writeout_string = '\t'.join(writeout)
-                    print("somatic mutation")
+
+                    #print("somatic mutation")
                     print(writeout_string)
-            if conc2 in spermdict:
-                
-                spermgeno = spermdict[conc2]
+            if conc2 in spermdict2:
+
+                spermgeno = spermdict2[conc2]
                 #print(spermgeno, geno2)
                 genotypes_minusspermgeno = genotypes2[0:10]+ genotypes2[12:16]
                 #print(genotypes_minusspermgeno)
@@ -276,32 +330,31 @@ for line2 in genos2: #now goes line by line in the list of matches from the seco
                 #print(justparentgenotypes)
                 justspermgenotypes = genotypes2[8:16]
                 #print(justspermgenotypes)
-                #print(genotypes2[0:2])
-                #print(genotypes2[10-15])
-                #print(genotypes_minusspermgeno)
-                #print(spermgeno, genotypes_minusspermgeno[2])
-                #print(spermdict)
-                #if spermgeno != genotypes_minusspermgeno[2]:
+
                 if not any(ele in spermgeno for ele in genotypes_minusspermgeno): # if the sperm genotype does not match the genotype of any other parent or sperm sample:
-                     #print(conc2)
+                     Type_of_Mutation = "UniqueGermlineMutation"
+                     Match = "NA"
                      #print(spermgeno, genotypes_minusspermgeno[2], conc2)
                      genolist = genotypes2[7:]
-                     writeout_uniqueglm = [conc2, ref, alt, genotypes2[16],genotypes2[17], genotypes2[18], genotypes2[19], genotypes2[20], genotypes2[21], genotypes2[22], genotypes2[23], genotypes2[24], genotypes2[25], genotypes2[26], genotypes2[27], genotypes2[28], genotypes2[29], genotypes2[30], genotypes2[31]]
+                     writeout_uniqueglm = [conc2, ref, alt, genotypes2[16],genotypes2[17], genotypes2[18], genotypes2[19], genotypes2[20], genotypes2[21], genotypes2[22], genotypes2[23], genotypes2[24], genotypes2[25], genotypes2[26], genotypes2[27], genotypes2[28], genotypes2[29], genotypes2[30], genotypes2[31], str(Match), str(Type_of_Mutation)]
 #                     #here is your writeout of all of the somatic mutations unique to a particular branch
                      writeout_uniqueglm_string = '\t'.join(writeout_uniqueglm)
-                     print("Unique germ line mutation")
+                     #print("Unique germ line mutation")
                      print(writeout_uniqueglm_string)
+
                 if all(x==justspermgenotypes[0] for x in justspermgenotypes) and not any(ele in spermgeno for ele in justparentgenotypes): # if the sperm genotype matches all other sperm genotypes but none of the parent genotypes:
-                    #print(conc2,justspermgenotypes)
+                    Type_of_Mutation = "GlobalGermlineMutation"
+                    Match = "NA"
                     genolist = genotypes2[7:]
-                    writeout_globalglm = [conc2, ref, alt, genotypes2[16],genotypes2[17], genotypes2[18], genotypes2[19], genotypes2[20], genotypes2[21], genotypes2[22], genotypes2[23], genotypes2[24], genotypes2[25], genotypes2[26], genotypes2[27], genotypes2[28], genotypes2[29], genotypes2[30], genotypes2[31]]
-                     #                     #here is your writeout of all of the somatic mutations unique to a particular branch
+                    writeout_globalglm = [conc2, ref, alt, genotypes2[16],genotypes2[17], genotypes2[18], genotypes2[19], genotypes2[20], genotypes2[21], genotypes2[22], genotypes2[23], genotypes2[24], genotypes2[25], genotypes2[26], genotypes2[27], genotypes2[28], genotypes2[29], genotypes2[30], genotypes2[31], str(Match), str(Type_of_Mutation)]
+                    #here is your writeout of all of the somatic mutations unique to a particular branch
+
                     writeout_globalglm_string = '\t'.join(writeout_globalglm)
-                    print("Global germ line mutation")
+                    #print("Global germ line mutation")
                     print(writeout_globalglm_string)
 # #                     print(conc2)
                # print(genotypes_minusspermgeno)
                 # if spermgeno !=
-                
-                
-                      
+
+
+
